5385d90cc4956a9c1a220c931ed0589e88fcf81e
Muhammed Modibbo <modibbomuhammed@gmail.com>
2021-05-29 16:27:22 +0100

    showpage done

--- public/javascripts/imageSlider.js
+++ public/javascripts/imageSlider.js
@@ -1,4 +1,52 @@
 const displayPrice = document.getElementById("displayPrice");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+const dots = document.querySelectorAll(".dot");
+let slideIndex = 1;
+
+dots.forEach(function (val) {
+  val.addEventListener("click", function (e) {
+    currentSlide(Number(val.id));
+  });
+});
+
+prevButton.addEventListener("click", () => {
+  plusSlides(-1);
+});
+
+nextButton.addEventListener("click", () => {
+  plusSlides(1);
+});
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n + 1));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
 
 function convert(numberString) {
   const length = numberString.length;
@@ -16,5 +64,3 @@ function convert(numberString) {
 }
 
 displayPrice.textContent = convert(displayPrice.textContent);
-
-
